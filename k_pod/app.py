e8e23a8209a582f4e1cdc0caf4c92fc58878fe34
vagrant <vagrant@controlplane>
2024-12-26 11:42:16 +0000

    new

--- k_pod/app.py
+++ k_pod/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.get("/new")
 def get_grettings():
-    greet_message = "Hi DevOps team , This is coming from container"
+    greet_message = "Hi DevOps team , Welcome"
     print(f"get_grettings() :: message is {greet_message}")
     return greet_message, 200
